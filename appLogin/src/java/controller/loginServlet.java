c3cdbb16f3c57707c0108f0f66a453b268450367
GLKaiky <kaikyfranca8@gmail.com>
2023-05-08 16:55:36 -0300

    Ainda nao funcional

--- appLogin/src/java/controller/loginServlet.java
+++ appLogin/src/java/controller/loginServlet.java
@@ -4,13 +4,39 @@
  */
 package controller;
 
+import java.awt.Color;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @WebServlet("/loginServlet")
  * @author 893138
  */
 public class loginServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) 
+        throws ServletException, IOException {
+        String id = req.getParameter("nome");
+        String pass = req.getParameter("senha");
+        
+        if (id.equals("Aluno")&&pass.equals("12345")){
+            RequestDispatcher rd = null;
+            
+            rd = req.getRequestDispatcher("/view/welcomer.jsp");
+            
+            req.setAttribute(id, "nome");
+           
+        }else {
+            PrintWriter out = resp.getWriter();
+            out.close();
+        }
+    }
     
-   dopost
+  
 }
