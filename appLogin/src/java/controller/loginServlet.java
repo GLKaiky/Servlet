5e32bf897fc8b4877aa28b96b95c8e364f660967
GLKaiky <kaikyfranca8@gmail.com>
2023-05-08 16:24:49 -0300

    ue

--- appLogin/src/java/controller/loginServlet.java
+++ appLogin/src/java/controller/loginServlet.java
@@ -3,19 +3,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package controller;
-                import java.io.IOException;
-		import java.io.PrintWriter;
 
-		import javax.servlet.ServletException;
-		import javax.servlet.annotation.WebServlet;
-		import javax.servlet.http.HttpServlet;
-		import javax.servlet.http.HttpServletRequest;
-		import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServlet;
+
 /**
- *
+ * @WebServlet("/loginServlet")
  * @author 893138
  */
-public class loginServlet {
+public class loginServlet extends HttpServlet {
     
-    
+   dopost
 }
